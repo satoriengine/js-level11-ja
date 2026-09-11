a55d790123d08676d72c8079c901ca8f74514614
Hiroshi Fujiwara <hiroshi.829f@gmail.com>
2026-09-11 06:06:13 +0000

    added chapter05

---
+++ chapter05/src/main.js
@@ -0,0 +1,16 @@
+import './style.css';
+import { setupCounter } from './counter.js';
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <h1>chapter05</h1>
+    <div>
+      <button id="counter" type="button"></button>
+    </div>
+    <p>
+      We are studying JavaScript
+    </p>
+  </div>
+`;
+
+setupCounter(document.querySelector('#counter'));
